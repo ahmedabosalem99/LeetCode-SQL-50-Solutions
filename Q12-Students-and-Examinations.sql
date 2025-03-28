6cef7af5c64cf63ad6522546d641f1aaac3345b5
Ahmed Abosalem <66332650+ahmedabosalem99@users.noreply.github.com>
2025-03-28 22:55:41 +0200

    Update and rename Students-and-Examinations.tsql to Q12-Students-and-Examinations.sql

--- Q12-Students-and-Examinations.sql
+++ Q12-Students-and-Examinations.sql
@@ -1,3 +1,5 @@
+--Problem Link: https://leetcode.com/problems/students-and-examinations/description/?envType=study-plan-v2&envId=top-sql-50
+
 SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.subject_name) AS attended_exams
 FROM Students s
 CROSS JOIN Subjects sub
